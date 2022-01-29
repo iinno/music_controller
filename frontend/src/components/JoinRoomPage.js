1e1216ae0fd6033c40c8b1a82f5f8fb26d6b3add
Vincent Kim <vince.inho@gmail.com>
2022-01-28 16:46:16 -0800

    Create JoinRoomView to check whether a room exists and if so redirect the user to the room. Create JoinRoomPage frontend component to provide a page for users to join a room.

--- frontend/src/components/JoinRoomPage.js
+++ frontend/src/components/JoinRoomPage.js
@@ -1,11 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link, useNavigate } from "react-router-dom";
 
-export default class JoinRoomPage extends Component {
-    constructor(props) {
-        super(props);
-    }
+function JoinRoomPage() {
+    let navigate = useNavigate();
+    const [roomCode, setRoomCode] = useState("");
+    const [error, setErrorCode] = useState("");
 
-    render() {
-        return <p>This is the join room page.</p>
-    }
-}
+    const handleTextFieldChange = e => {
+        setRoomCode(e.target.value);
+    };
+
+    const handleRoomButtonPressed = () => {
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                code: roomCode
+            })
+        };
+
+        fetch("/api/join-room", requestOptions)
+        .then(response => {
+            if (response.ok) {
+                navigate("/room/" + roomCode)
+            } else {
+                setErrorCode("Room not found")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
+
+    return (
+        <Grid container spacing={1} align="center" direction="column">
+            <Grid item xs={12}>
+                <Typography var="h4" component="h4">
+                    Join a Room
+                </Typography>
+            </Grid>
+            <Grid item xd={12}>
+                <TextField 
+                    error={error}
+                    label="Code"
+                    placeholder="Enter a Room Code."
+                    value={roomCode}
+                    helperText={error}
+                    variant="outlined"
+                    onChange={handleTextFieldChange}
+                />
+            </Grid>
+            <Grid item xd={12}>
+                <Button variant="contained" color="primary" onClick={handleRoomButtonPressed}>
+                    Enter Room
+                </Button>
+                <Button variant="contained" color="secondary" to="/" component={Link}>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+export default JoinRoomPage;
