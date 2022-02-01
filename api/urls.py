87a21ad00e226c92b9f4837385996cdab4961956
iinno <vince.inho@gmail.com>
2022-01-31 23:54:08 -0800

    Add functionality to Room.js (leave room), add a leaveroom view in the endpoint to delete rooms, handle redirecting when room no longer exists, add grid for room page.

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, userInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, userInRoom, LeaveRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', userInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view()),
 ]
